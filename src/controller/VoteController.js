61a8f55d93232adcca5b889a033436a33bdda1c7
Batschko <lukas.h2@hotmail.de>
2021-03-09 18:23:16 +0100

    added vote stubs

--- src/controller/VoteController.js
+++ src/controller/VoteController.js
@@ -5,15 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var router = express_1.default.Router();
-router.get("/", function (req, res) {
-    var username = req.header("name");
-    if (username == undefined || username == "") {
-        return res.status(404).json({ "error": "ID unknown" });
+router.post("/", function (req, res) {
+    var wishId = req.header("wishId");
+    var vote = req.header("vote");
+    if (wishId == undefined || wishId == "" || vote == undefined || vote == "") {
+        return res.status(404).json({ "error": "required field undefined" });
     }
     //database res.status(400).json({"error": "ID couldnt be processed"})
     var json = {
+        "msg": "Vote cased",
         "arguments": {
-            "name": username
+            "wishId": wishId,
+            "vote": vote
         }
     };
     return res.status(200).json(json);
