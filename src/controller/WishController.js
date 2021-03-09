a837e808f3ef42329188fde97e35f054b5c7708b
Batschko <lukas.h2@hotmail.de>
2021-03-09 18:22:58 +0100

    added wish stubs

--- src/controller/WishController.js
+++ src/controller/WishController.js
@@ -6,14 +6,131 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var router = express_1.default.Router();
 router.get("/", function (req, res) {
-    var username = req.header("name");
-    if (username == undefined || username == "") {
-        return res.status(404).json({ "error": "ID unknown" });
+    var userId = req.header("userId");
+    var day = req.header("day");
+    var month = req.header("month");
+    var json = undefined;
+    if (userId == undefined || userId == "") {
+        return res.status(404).json({ "error": "required field undefined" });
+    }
+    if (day != undefined && day != "") {
+        if (month == undefined || month == "") {
+            return res.status(404).json({ "error": "month undefined" });
+        }
+        json = [{
+                "arguments": {
+                    "userId": userId,
+                    "day": day,
+                    "month": month
+                }
+            },
+            {
+                "id": "a4b13f26-a617-4303-9a63-a74c1e44d233",
+                "name": "Bolognese",
+                "groupname": "Familie Cage",
+                "day": "24-12-2020",
+                "daytime": "lunch",
+                "votes": {
+                    "positive": 5,
+                    "negative": 1
+                }
+            },
+            {
+                "id": "2ea16774-18dd-40b7-b724-bd2505b83ae0",
+                "name": "Nudelauflauf",
+                "groupname": "Familie Spacey",
+                "day": "24-12-2020",
+                "daytime": "evening",
+                "votes": {
+                    "positive": 4,
+                    "negative": 0
+                }
+            }
+        ];
+    }
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    if (json == undefined) {
+        json = [{
+                "arguments": {
+                    "userId": userId,
+                    "day": day,
+                    "month": month
+                }
+            },
+            {
+                "id": "a4b13f26-a617-4303-9a63-a74c1e44d233",
+                "name": "Bolognese",
+                "groupname": "Familie Cage",
+                "day": "24-12-2020",
+                "daytime": "lunch",
+                "votes": {
+                    "positive": 5,
+                    "negative": 1
+                }
+            },
+            {
+                "id": "2ea16774-18dd-40b7-b724-bd2505b83ae0",
+                "name": "Nudelauflauf",
+                "groupname": "Familie Spacey",
+                "day": "24-12-2020",
+                "daytime": "evening",
+                "votes": {
+                    "positive": 4,
+                    "negative": 0
+                }
+            }
+        ];
+    }
+    return res.status(200).json(json);
+});
+router.post("/", function (req, res) {
+    var groupId = req.header("groupId");
+    var dishId = req.header("dishId");
+    var daytime = req.header("daytime");
+    if (groupId == undefined || groupId == "" || dishId == undefined || dishId == "" || daytime == undefined || daytime == "") {
+        return res.status(404).json({ "error": "required field undefined" });
     }
     //database res.status(400).json({"error": "ID couldnt be processed"})
     var json = {
+        "msg": "Wish created",
         "arguments": {
-            "name": username
+            "groupId": groupId,
+            "dishId": dishId,
+            "daytime": daytime
+        }
+    };
+    return res.status(200).json(json);
+});
+router.put("/", function (req, res) {
+    var wishId = req.header("wishId");
+    var groupId = req.header("groupId");
+    var dishId = req.header("dishId");
+    var daytime = req.header("daytime");
+    if (wishId == undefined || wishId == "") {
+        return res.status(404).json({ "error": "required field undefined" });
+    }
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    var json = {
+        "msg": "Wish updated",
+        "arguments": {
+            "wishId": wishId,
+            "groupId": groupId,
+            "dishId": dishId,
+            "daytime": daytime
+        }
+    };
+    return res.status(200).json(json);
+});
+router.delete("/", function (req, res) {
+    var wishId = req.header("wishId");
+    if (wishId == undefined || wishId == "") {
+        return res.status(404).json({ "error": "required field undefined" });
+    }
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    var json = {
+        "msg": "Wish deleted",
+        "arguments": {
+            "wishId": wishId
         }
     };
     return res.status(200).json(json);
--- src/controller/WishController.ts
+++ src/controller/WishController.ts
@@ -1,20 +1,147 @@
 import express from "express";
+
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    const username = req.header("name");
-    if (username == undefined || username == ""){
-        return res.status(404).json({"error": "ID unknown"})
+router.get("/", function (req, res) {
+    const userId = req.header("userId");
+    const day = req.header("day");
+    const month = req.header("month");
+    let json = undefined;
+    if (userId == undefined || userId == "") {
+        return res.status(404).json({"error": "required field undefined"})
+    }
+    if (day != undefined && day != "") {
+        if (month == undefined || month == "") {
+            return res.status(404).json({"error": "month undefined"})
+        }
+        json = [{
+            "arguments": {
+                "userId": userId,
+                "day": day,
+                "month": month
+            }
+        },
+            {
+                "id": "a4b13f26-a617-4303-9a63-a74c1e44d233",
+                "name": "Bolognese",
+                "groupname": "Familie Cage",
+                "day": "24-12-2020",
+                "daytime": "lunch",
+                "votes": {
+                    "positive": 5,
+                    "negative": 1
+                }
+            },
+            {
+                "id": "2ea16774-18dd-40b7-b724-bd2505b83ae0",
+                "name": "Nudelauflauf",
+                "groupname": "Familie Spacey",
+                "day": "24-12-2020",
+                "daytime": "evening",
+                "votes": {
+                    "positive": 4,
+                    "negative": 0
+                }
+            }
+        ]
+
     }
     //database res.status(400).json({"error": "ID couldnt be processed"})
-    let json={
+    if (json == undefined) {
+        json = [{
+            "arguments": {
+                "userId": userId,
+                "day": day,
+                "month": month
+            }
+        },
+            {
+                "id": "a4b13f26-a617-4303-9a63-a74c1e44d233",
+                "name": "Bolognese",
+                "groupname": "Familie Cage",
+                "day": "24-12-2020",
+                "daytime": "lunch",
+                "votes": {
+                    "positive": 5,
+                    "negative": 1
+                }
+            },
+            {
+                "id": "2ea16774-18dd-40b7-b724-bd2505b83ae0",
+                "name": "Nudelauflauf",
+                "groupname": "Familie Spacey",
+                "day": "24-12-2020",
+                "daytime": "evening",
+                "votes": {
+                    "positive": 4,
+                    "negative": 0
+                }
+            }
+        ]
+    }
+
+
+    return res.status(200).json(json);
+});
+
+router.post("/", function (req, res) {
+    const groupId = req.header("groupId");
+    const dishId = req.header("dishId");
+    const daytime = req.header("daytime");
+    if (groupId == undefined || groupId == "" || dishId == undefined || dishId == "" || daytime == undefined || daytime == "") {
+        return res.status(404).json({"error": "required field undefined"})
+    }
+
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    let json = {
+        "msg": "Wish created",
         "arguments": {
-            "name": username
+            "groupId": groupId,
+            "dishId": dishId,
+            "daytime": daytime
         }
     }
 
     return res.status(200).json(json);
 });
 
+router.put("/", function (req, res) {
+    const wishId  = req.header("wishId");
+    const groupId  = req.header("groupId");
+    const dishId = req.header("dishId");
+    const daytime = req.header("daytime");
+    if (wishId == undefined || wishId == "") {
+        return res.status(404).json({"error": "required field undefined"})
+    }
 
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    let json = {
+        "msg": "Wish updated",
+        "arguments": {
+            "wishId": wishId,
+            "groupId": groupId,
+            "dishId": dishId,
+            "daytime": daytime
+        }
+    }
+
+    return res.status(200).json(json);
+});
+
+router.delete("/", function (req, res) {
+    const wishId  = req.header("wishId");
+    if (wishId == undefined || wishId == "") {
+        return res.status(404).json({"error": "required field undefined"})
+    }
+
+    //database res.status(400).json({"error": "ID couldnt be processed"})
+    let json = {
+        "msg": "Wish deleted",
+        "arguments": {
+            "wishId": wishId
+        }
+    }
+
+    return res.status(200).json(json);
+});
 module.exports = router;
