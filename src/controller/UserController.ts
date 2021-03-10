29e35ff3bde0c14c560770cac3c2774a2f0e588a
Batschko <lukas.h2@hotmail.de>
2021-03-11 00:11:30 +0100

    started impl user post

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import {User} from "../model/user/User";
+import {Dish} from "../model/food/dish/Dish";
+import {Ingredient} from "../model/food/ingredients/Ingredient";
+import {UserGroup} from "../model/user/UserGroup";
+import {Vote} from "../model/user/vote/Vote";
+import {ShoppingList} from "../model/shoppinglist/ShoppingList";
+import {getConnection} from "typeorm/index";
 const router = express.Router();
 
 router.get("/:name", function(req, res) {
@@ -21,7 +28,7 @@ router.get("/:name", function(req, res) {
 });
 
 
-router.post("/", function(req, res) {
+router.post("/", async function(req, res) {
     const firstname = req.header("firstname");
     const lastname = req.header("lastname");
     const username = req.header("username");
@@ -31,24 +38,26 @@ router.post("/", function(req, res) {
     if(firstname == undefined || lastname == undefined || username == undefined || email == undefined || password == undefined || birthdate == undefined){
         return res.status(400).json({"error": "required field undefined"});
     }
-    let json={
-        "msg": "User created",
-        "arguments": {
-            "firstname": firstname,
-            "lastname": lastname,
-            "username": username,
-            "email": email,
-            "password": password,
-            "birthdate": birthdate
-        }
+//TODO es gibt keine fileurl von
+    let user = new User("help i need some id",firstname,lastname,email,birthdate,username,"https://cdn.vox-cdn.com/thumbor/G99dGM7X_R1gjLV7OF-bPuoP4GY=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/18969934/Screen_Shot_2019_08_13_at_3.47.16_PM.png"
+        ,undefined as unknown as Dish[],
+        undefined as unknown as Ingredient[],
+        undefined as unknown as UserGroup[],
+        undefined as unknown as Vote[],
+        undefined as unknown as ShoppingList[]
+    )
+    try{
+        await getConnection().getRepository(UserGroup).manager.save(user)
+    }catch (e){
+        console.log(e)
+        return res.status(400).json({"error": "Error at db access"});
     }
+    let json = {
+        "msg": "User created"
+    };
 
-    let success = true;
-    if(success){
-        return res.status(200).json(json);
-    }else{
-        return res.status(400).json({"error": "ID couldnt be processed"});
-    }
+    return res.status(200).json(json);
+
 
 });
 
