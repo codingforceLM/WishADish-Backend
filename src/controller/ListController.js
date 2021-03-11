38aa62f393b06020e4c38cf8ddaff95bb71527bc
Batschko <lukas.h2@hotmail.de>
2021-03-11 17:53:46 +0100

    started post impl list

--- src/controller/ListController.js
+++ src/controller/ListController.js
@@ -1,62 +1,99 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var index_1 = require("typeorm/index");
+var ShoppingList_1 = require("../model/shoppinglist/ShoppingList");
 var router = express_1.default.Router();
 router.get("/", function (req, res) {
-    var userId = req.header("userId");
-    var done = req.header("done");
-    if (userId == undefined || userId == "") {
-        return res.status(404).json({ "error": "required field undefined" });
-    }
-    //database res.status(400).json({"error": "ID couldnt be processed"})
-    var json = [{
-            "arguments": {
-                "userId": userId,
-                "done": done
+    return __awaiter(this, void 0, void 0, function () {
+        var groupId, done, results, e_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    groupId = req.header("groupId");
+                    done = req.header("done");
+                    if (groupId == undefined || groupId == "") {
+                        return [2 /*return*/, res.status(404).json({ "error": "required field undefined" })];
+                    }
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, index_1.getConnection().getRepository(ShoppingList_1.ShoppingList).find({
+                            relations: ['_shoppingListIngredients'],
+                            where: { _id: groupId }
+                        })];
+                case 2:
+                    results = (_a.sent());
+                    return [3 /*break*/, 4];
+                case 3:
+                    e_1 = _a.sent();
+                    console.log(e_1);
+                    return [2 /*return*/, res.status(400).json({ "error": "Unknown groupId" })];
+                case 4:
+                    if (results == undefined || results == []) {
+                        return [2 /*return*/, res.status(400).json({ "error": "Error at db access" })];
+                    }
+                    /*
+                        let json = [];
+                        let userlist = [];
+                    
+                        for(let i=0; i<results.length;i++){
+                            userlist.push({
+                                "id": results[i].user.id,
+                                "name": results[i].user.firstname + " " + results[i].user.lastname,
+                                "role": results[i].role
+                            })
+                        }
+                    
+                        json.push({
+                            "id": results[0].group.id,
+                            "name": results[0].group.title,
+                            "user": userlist
+                        })
+                    */
+                    return [2 /*return*/, res.status(200).json(results)];
             }
-        },
-        {
-            "id": "273e8601-beea-414b-a771-7495a8a416d4",
-            "name": "Cage's list",
-            "ingredients": [
-                {
-                    "id": "b625bcbc-a085-4f88-9ae2-2ba50c64644f",
-                    "name": "Zwiebel",
-                    "done": false
-                },
-                {
-                    "id": "be07d3c1-8526-443d-97c1-f4ff2bf3d1dd",
-                    "name": "Tomate",
-                    "done": true
-                }
-            ]
-        },
-        {
-            "id": "273e8601-beea-414b-a771-7495a8a416d4",
-            "name": "Cage's list",
-            "ingredients": [
-                {
-                    "id": "c89f1005-3960-4107-bfda-97228231cdda",
-                    "name": "Mehl",
-                    "done": false
-                },
-                {
-                    "id": "f7886fad-b3e1-4748-a942-6209c60295de",
-                    "name": "Zucker",
-                    "done": false
-                },
-                {
-                    "id": "2b752bfe-14b2-45c8-b6da-a08d1427ba42",
-                    "name": "Backpulver",
-                    "done": true
-                }
-            ]
-        }
-    ];
-    return res.status(200).json(json);
+        });
+    });
 });
 router.post("/", function (req, res) {
     var name = req.header("name");
--- src/controller/ListController.ts
+++ src/controller/ListController.ts
@@ -1,63 +1,53 @@
 import express from "express";
+import {getConnection} from "typeorm/index";
+import {ShoppingList} from "../model/shoppinglist/ShoppingList";
+import {Group} from "../model/user/group/Group";
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-    const userId = req.header("userId");
+router.get("/", async function (req, res) {
+    const groupId = req.header("groupId");
     const done = req.header("done");
-    if (userId == undefined || userId == "") {
+    if (groupId == undefined || groupId == "") {
         return res.status(404).json({"error": "required field undefined"});
     }
-    //database res.status(400).json({"error": "ID couldnt be processed"})
-    let json =
-        [{
-            "arguments": {
-                "userId": userId,
-                "done": done
-            }
-        },
+    let results;
+    try{
+        results = await getConnection().getRepository(ShoppingList).find(
             {
-                "id": "273e8601-beea-414b-a771-7495a8a416d4",
-                "name": "Cage's list",
-                "ingredients": [
-                    {
-                        "id": "b625bcbc-a085-4f88-9ae2-2ba50c64644f",
-                        "name": "Zwiebel",
-                        "done": false
-                    },
-                    {
-                        "id": "be07d3c1-8526-443d-97c1-f4ff2bf3d1dd",
-                        "name": "Tomate",
-                        "done": true
-                    }
-                ]
-            },
-            {
-                "id": "273e8601-beea-414b-a771-7495a8a416d4",
-                "name": "Cage's list",
-                "ingredients": [
-                    {
-                        "id": "c89f1005-3960-4107-bfda-97228231cdda",
-                        "name": "Mehl",
-                        "done": false
-                    },
-                    {
-                        "id": "f7886fad-b3e1-4748-a942-6209c60295de",
-                        "name": "Zucker",
-                        "done": false
-                    },
-                    {
-                        "id": "2b752bfe-14b2-45c8-b6da-a08d1427ba42",
-                        "name": "Backpulver",
-                        "done": true
-                    }
-                ]
-            }
-        ]
+                relations: ['_shoppingListIngredients'],
+                where:
+                    {_id: groupId}
+            }) as ShoppingList[];
+    }catch(e) {
+        console.log(e);
+        return res.status(400).json({"error": "Unknown groupId"});
+    }
 
+    if(results == undefined || results == []) {
+        return res.status(400).json({"error": "Error at db access"});
+    }
+/*
+    let json = [];
+    let userlist = [];
 
-    return res.status(200).json(json);
+    for(let i=0; i<results.length;i++){
+        userlist.push({
+            "id": results[i].user.id,
+            "name": results[i].user.firstname + " " + results[i].user.lastname,
+            "role": results[i].role
+        })
+    }
+
+    json.push({
+        "id": results[0].group.id,
+        "name": results[0].group.title,
+        "user": userlist
+    })
+*/
+    return res.status(200).json(results);
 });
+
 
 router.post("/", function (req, res) {
     const name = req.header("name");
